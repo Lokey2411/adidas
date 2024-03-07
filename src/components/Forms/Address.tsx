62f8e37a8a632cc80bd4d357b6829c5ef8790ed7
lokey2411 <hahaiviet24112003@gmail.com>
2024-03-07 23:23:56 +0700

    update user interact

--- src/components/Forms/Address.tsx
+++ src/components/Forms/Address.tsx
@@ -9,6 +9,7 @@ type FormState = {
 const Address = () => {
 	const { user, setUser } = useContext(UserContext);
 	const [isOpenModel, setIsOpenModel] = useState(false);
+	const [isOpenEditModel, setisOpenEditModel] = useState([false, false, false]);
 	const [formState, setFormState] = useState<FormState>({
 		fullName: user.name,
 		street: "",
@@ -25,22 +26,34 @@ const Address = () => {
 			[e.target.name]: e.target.value,
 		});
 	};
-	const onSubmit = () => {
-		if (Object.values(formState).includes("")) {
-			return;
+	const onSubmit = (index = -1) => {
+		if (index > 0) {
+			const newUser = {
+				building: formState.building,
+				city: formState.city,
+				street: formState.street,
+			};
+			let newUserAddress = user.address;
+			newUserAddress[index] = newUser;
+			setUser({ ...user, address: newUserAddress });
+			let newModels = isOpenEditModel;
+			newModels[index] = !isOpenEditModel[index];
+			setisOpenEditModel(newModels);
+		} else {
+			setUser({
+				...user,
+				address: [
+					...user.address,
+					{
+						building: formState.building,
+						city: formState.city,
+						street: formState.street,
+					},
+				],
+			});
+			setisOpenEditModel([...isOpenEditModel, false]);
+			setIsOpenModel(false);
 		}
-		setUser({
-			...user,
-			address: [
-				...user.address,
-				{
-					building: formState.building,
-					city: formState.city,
-					street: formState.street,
-				},
-			],
-		});
-		setIsOpenModel(false);
 	};
 	const onCancel = () => {
 		setIsOpenModel(false);
@@ -50,7 +63,187 @@ const Address = () => {
 			<h2 className="font-bold text-2xl">Sổ địa chỉ</h2>
 			<p className="text-sm my-2">Bạn còn {5 - user.address.length}/5 ô địa chỉ.</p>
 			{user.address.map((item, index) => (
-				<div className="border border-black rounded-lg mb-6">
+				<div
+					className="border border-black rounded-lg mb-6"
+					key={index}
+				>
+					<BoxModel
+						isOpenModel={isOpenEditModel[index]}
+						setIsOpenModel={(value: boolean) => {
+							let newModels = isOpenEditModel;
+							newModels[index] = value;
+							setisOpenEditModel(newModels);
+						}}
+					>
+						<div className="bg-white rounded-md w-1/2 h-screen p-6">
+							<h1 className="text-xl font-bold text-center">Thay đổi địa chỉ</h1>
+							<form
+								action=""
+								className="flex flex-col  justify-between h-[70%] my-6"
+							>
+								<input
+									className="w-full border border-black rounded-lg p-2"
+									type="text"
+									name="fullName"
+									id=""
+									placeholder="Họ và tên"
+									value={formState.fullName}
+									onChange={onFormStateChange}
+									disabled
+								/>
+								{!formState.fullName && (
+									<label
+										htmlFor="fullName"
+										className="text-red-500"
+									>
+										Yêu cầu nhập
+									</label>
+								)}
+								<input
+									className="w-full border border-black rounded-lg p-2"
+									type="text"
+									name="street"
+									id=""
+									placeholder="Số nhà / Tên đường * "
+									value={formState.street}
+									onChange={onFormStateChange}
+								/>
+								{!formState.street && (
+									<label
+										htmlFor="street"
+										className="text-red-500"
+									>
+										Yêu cầu nhập
+									</label>
+								)}
+								<input
+									className="w-full border border-black rounded-lg p-2"
+									type="text"
+									name="building"
+									id=""
+									placeholder="Tòa nhà / Số tầng "
+									value={formState.building}
+									onChange={onFormStateChange}
+								/>
+								{!formState.building && (
+									<label
+										htmlFor="building"
+										className="text-red-500"
+									>
+										Yêu cầu nhập
+									</label>
+								)}
+								<div className="grid grid-cols-2 gap-6">
+									<div className="">
+										<input
+											className="w-full border border-black rounded-lg p-2"
+											type="text"
+											name="city"
+											id=""
+											placeholder="Thành phố/ Tỉnh "
+											value={formState.city}
+											onChange={onFormStateChange}
+										/>
+										{!formState.city && (
+											<label
+												htmlFor="city"
+												className="text-red-500"
+											>
+												Yêu cầu nhập
+											</label>
+										)}
+									</div>
+									<div className="">
+										<input
+											className="w-full border border-black rounded-lg p-2"
+											type="text"
+											name="strict"
+											id=""
+											placeholder="Quận"
+											value={formState.strict}
+											onChange={onFormStateChange}
+										/>
+										{!formState.strict && (
+											<label
+												htmlFor="strict"
+												className="text-red-500"
+											>
+												Yêu cầu nhập
+											</label>
+										)}
+									</div>
+									<div className="">
+										<input
+											className="w-full border border-black rounded-lg p-2"
+											type="text"
+											name="guild"
+											id=""
+											placeholder="Phường  "
+											value={formState.guild}
+											onChange={onFormStateChange}
+										/>
+										{!formState.guild && (
+											<label
+												htmlFor="guild"
+												className="text-red-500"
+											>
+												Yêu cầu nhập
+											</label>
+										)}
+									</div>
+									<div className="">
+										<input
+											className="w-full border border-black rounded-lg p-2"
+											type="text"
+											name="code"
+											id=""
+											placeholder="Mã bưu chính * "
+											value={formState.code}
+											onChange={onFormStateChange}
+										/>
+										{!formState.code && (
+											<label
+												htmlFor="code"
+												className="text-red-500"
+											>
+												Yêu cầu nhập
+											</label>
+										)}
+									</div>
+								</div>
+								<input
+									className="w-full border border-black rounded-lg p-2"
+									type="text"
+									name="phoneNumber"
+									id=""
+									placeholder="Tòa nhà / Số tầng "
+									value={formState.phoneNumber}
+									disabled
+									onChange={onFormStateChange}
+								/>
+								{!formState.phoneNumber && (
+									<label
+										htmlFor="phoneNumber"
+										className="text-red-500"
+									>
+										Yêu cầu nhập
+									</label>
+								)}
+							</form>
+							<button
+								className="border border-black rounded-md py-2 px-4 font-medium"
+								onClick={() => onSubmit(index)}
+							>
+								Lưu
+							</button>
+							<button
+								className="border border-black rounded-md py-2 px-4 ml-3 font-medium"
+								onClick={onCancel}
+							>
+								Hủy
+							</button>
+						</div>
+					</BoxModel>
 					<div
 						key={index}
 						className="p-4"
@@ -61,7 +254,16 @@ const Address = () => {
 						<p className="text-md">{item.city}</p>
 						<p>{user.phoneNumber}</p>
 						<div className="flex">
-							<button className="cursor-pointer border border-black rounded-lg px-4 py-2 mr-4">Chỉnh sửa</button>
+							<button
+								className="cursor-pointer border border-black rounded-lg px-4 py-2 mr-4"
+								onClick={() => {
+									let newModels = isOpenEditModel;
+									newModels[index] = !isOpenEditModel[index];
+									setisOpenEditModel(newModels);
+								}}
+							>
+								Chỉnh sửa
+							</button>
 							<button className="cursor-pointer border border-black rounded-lg px-4 py-2 mr-4">Xóa</button>
 						</div>
 					</div>
@@ -237,7 +439,7 @@ const Address = () => {
 					</form>
 					<button
 						className="border border-black rounded-md py-2 px-4 font-medium"
-						onClick={onSubmit}
+						onClick={() => onSubmit()}
 					>
 						Thêm
 					</button>
