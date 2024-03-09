24b95d7b3bca4fdabb6fa9e1404c4c045364382d
vanh1604 <VietAnhScout@gmail.com>
2024-03-09 17:55:21 +0700

    finish screen cart, Pay and thank

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Root from "./routes/Root";
-
+import PayScreen from "./pages/PayScreen";
+import CartScreen from "./pages/CartScreen";
+import ThankForOrder from "./components/Layout/ThankForOrder";
 function App() {
-	return <Root />;
+  return <ThankForOrder />;
 }
 
 export default App;
---
+++ src/pages/CartScreen.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Navbar from "../components/Layout/Navbar";
+import Contact from "../components/Layout/Contact";
+import Footer from "../components/Layout/Footer";
+
+function CartScreen() {
+  return (
+    <div>
+      <Navbar />
+      <div className="">
+        <p className="font-bold">Giỏ hàng</p>
+      </div>
+      <div className="flex w-full justify-around items-center border-solid border-2 border-black">
+        <div className="w-1/2">
+          <img
+            src="https://picsum.photos/200/200"
+            alt=""
+            className="w-[200px] h-[200px] object-cover p-3"
+          />
+        </div>
+        <div className="w-1/2  ">
+          <div>
+            <p className="font-bold">Country og sftm</p>
+          </div>
+          <div>
+            <p>
+              Price : <span>3.000.000</span>
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="">Số lượng</label>
+            <input className="border w-1/5" type="number" />
+          </div>
+          <div>
+            <button>Xóa</button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <button className="flex justify-center bg-white text-black hover:opacity-70 hover:bg-red-500 rounded-lg px-8 py-3 font-semibold mx-auto text-xl w-1/7 mb-4 cursor-pointer">
+          Thanh toán
+        </button>
+      </div>
+      <Contact/>
+      <Footer />
+    </div>
+  );
+}
+
+export default CartScreen;
